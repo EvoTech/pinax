9639d9669ec788a773144963bfe802788a2fac2b
EvoTech <iv_zak@yahoo.com>
2012-05-16 05:34:54 +0300

    LoginRequireMiddleware added but not tested yet

--- pinax/middleware/security.py
+++ pinax/middleware/security.py
@@ -1,10 +1,36 @@
 import re
 
 from django.conf import settings
+from django.contrib.auth.views import redirect_to_login
 from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import resolve
 
-MASK_IN_EXCEPTION_EMAIL= ["password", "mail", "protected", "private"]
+MASK_IN_EXCEPTION_EMAIL = ["password", "mail", "protected", "private"]
 mask_re = re.compile("(" + "|".join(MASK_IN_EXCEPTION_EMAIL) + ")", re.I)
+
+
+DEFAULT_LOGIN_REQUIRE_URL_EXCEPTIONS = [
+    "acct_login",
+    "acct_signup",
+    "acct_confirm_email",
+    "acct_passwd_reset",
+    "acct_passwd_reset_done",
+    "acct_passwd_reset_key",
+]
+
+DEFAULT_LOGIN_REQUIRE_REGEXP_EXCEPTIONS = []
+
+LOGIN_REQUIRE_URL_EXCEPTIONS = getattr(
+    settings,
+    'LOGIN_REQUIRE_URL_EXCEPTIONS',
+    DEFAULT_LOGIN_REQUIRE_URL_EXCEPTIONS
+)
+
+LOGIN_REQUIRE_REGEXP_EXCEPTIONS = getattr(
+    settings,
+    'LOGIN_REQUIRE_REGEXP_EXCEPTIONS',
+    DEFAULT_LOGIN_REQUIRE_REGEXP_EXCEPTIONS
+)
 
 
 class HideSensistiveFieldsMiddleware(object):
@@ -12,7 +38,6 @@ class HideSensistiveFieldsMiddleware(object):
     A middleware that masks sensitive fields when an exception occurs,
     e.g. passwords in login attempts.
     """
-    
     def process_exception(self, request, exception):
         if not request or not request.POST or settings.DEBUG:
             return None
@@ -27,6 +52,28 @@ class HideSensistiveFieldsMiddleware(object):
                 masked = True
         if hasattr(request.POST, "_mutable"):
             request.POST._mutable = mutable
+
+
+class LoginRequireMiddleware(object):
+    """Whole site requires login middleware."""
+    def __init__(self):
+        """Constructor."""
+        self.url_exceptions = LOGIN_REQUIRE_URL_EXCEPTIONS
+        self.regexp_exceptions = [re.compile(url) for url in LOGIN_REQUIRE_REGEXP_EXCEPTIONS]
+
+    def process_request(self, request):
+        """Determines, is current path require login."""
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            return None
+        # Looking by url names
+        match = resolve(request.path_info)
+        if match.view_name in self.url_exceptions:
+            return None
+        # Looking by regexp matching
+        for url in self.rexexp_exceptions:
+            if url.match(request.path_info):
+                return None
+        return redirect_to_login(request.get_full_path())
 
 
 class GroupPrivateMiddleware(object):
