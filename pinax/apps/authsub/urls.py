1d1db54698adfbd07b184c4447062a435f838ba2
EvoTech <ivzak@yandex.ru>
2013-10-08 04:41:57 +0300

    cleanup contacts

--- pinax/apps/authsub/urls.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns("",
-    url(r"^login/$", "pinax.apps.authsub.views.login", name="authsub_login"),
-)
