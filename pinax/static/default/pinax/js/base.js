e1a5c0bf9fd8af3fbc94a06095ca48cd0a5dc0b8
EvoTech <ivzak@yandex.ru>
2013-10-08 03:12:40 +0300

    html escape

--- pinax/static/default/pinax/js/base.js
+++ pinax/static/default/pinax/js/base.js
@@ -8,3 +8,21 @@ $(function() {
         return false;
     });
 });
+
+function htmlEncode(value){
+    if (value) {
+        // return jQuery('<div />').text(value).html(); // Do not use it, not handle quotes
+        return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+    } else {
+        return '';
+    }
+}
+ 
+function htmlDecode(value) {
+    if (value) {
+        // return $('<div />').html(value).text();
+        return value.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+    } else {
+        return '';
+    }
+}
