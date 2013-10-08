13a15b5e742a8372303be91b41095b7f3edd85dc
EvoTech <ivzak@yandex.ru>
2013-10-08 03:42:15 +0300

    rm wakawaka_reversion_noconflict

--- pinax/apps/wakawaka_reversion_noconflict/models.py
+++
@@ -1,8 +0,0 @@
-from wakawaka.models import Revision
-
-
-for field in Revision._meta.fields[:]:
-    if field.name in ('creator',):
-        field.rel.related_name = 'waka_revisions'
-
-Revision.creator.related_name = 'waka_revisions'
